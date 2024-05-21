eb0fe826198863ff77c3da73d43d8897bec69977
Tai Tran <transytai12a1@gmail.com>
2024-05-21 10:04:58 +0700

    Squashed 'cloud-developer-c4/' content from commit e8d835c
    git-subtree-dir: cloud-developer-c4
    git-subtree-split: e8d835c61c0aba803b81359d4cd10fa67b3bc033

---
+++ backend/src/lambda/http/deleteTodo.ts
@@ -0,0 +1,34 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors, httpErrorHandler } from 'middy/middlewares'
+
+import { deleteTodo } from '../../businessLayer/todos'
+import { getUserId } from '../utils'
+
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const todoId = event.pathParameters.todoId
+    // TODO(done): Remove a TODO item by id
+    const userId = getUserId(event)
+    deleteTodo(userId, todoId)
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: `Todo item #${todoId} deleted successfully`
+    }
+  }
+)
+
+handler
+  .use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: true
+    })
+  )
---
+++ backend/src/lambda/http/updateTodo.ts
@@ -0,0 +1,36 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors, httpErrorHandler } from 'middy/middlewares'
+
+import { updateTodo } from '../../businessLayer/todos'
+import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { getUserId } from '../utils'
+
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const todoId = event.pathParameters.todoId
+    const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+    // TODO(done): Update a TODO item with the provided id using values in the "updatedTodo" object
+    const userId = getUserId(event)
+    await updateTodo(userId, todoId, updatedTodo)
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: `Todo item #${todoId} updated successfully`
+    }
+  }
+)
+
+handler
+  .use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: true
+    })
+  )
